eb8307c5e7e6bfc07675e0513bb223eb6a9cc688
Sxmmie <samuelumohjnr@gmail..com>
2019-12-03 08:14:55 +0100

    Added client app
    Added React client app as the web app frontend, also used semantic-ui-react component.

---
+++ client-app/src/App.tsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { Header, Icon, List } from 'semantic-ui-react'
+import "./App.css";
+import axios from 'axios'
+
+class App extends Component {
+    state = {
+        weathers: []
+    };
+
+    // Wait till the component gets mounted and go fetch some data from API
+    componentDidMount() {
+        axios.get("localhost:5000/api/weathers")
+            .then((response) => {
+                this.setState({
+                    weathers: response.data
+                });
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <Header as='h2' icon='plug' content='Uptime Guarantee'>
+                    <Icon name="users"/>
+                    <Header.Content>Ellative</Header.Content>
+                </Header>
+                <List>
+                    {this.state.weathers.map((weather: any) => (
+                        <List.Item key={weather.id}>{weather.type}</List.Item>
+                    ))}
+                </List>
+            </div>
+        );
+    }
+}
+
+export default App;
